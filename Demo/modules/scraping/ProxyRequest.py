7b36eb6a4dda546af35c621d3fae5ba2f4bffb7a
Daniel Alonso Báscones <dab0012@alu.ubu.es>
2023-02-21 10:04:55 +0100

    Correction of small bugs in the ProxyRequest class, in turn the documentation and structure have been improved. #4

--- Demo/modules/scraping/ProxyRequest.py
+++ Demo/modules/scraping/ProxyRequest.py
@@ -1,41 +1,49 @@
-import requests
-import random
-from bs4 import BeautifulSoup
-from colorama import Fore, Style
-from typing import Dict
-
-# Class to handle HTTP requests in a more transparent way in scraping and denial of service environments
-# by the servers from which the data is requested
-# Basically, it manages the proxies and user agents so that scraping is not detected
-#
-# Usage example:
-# request_handler = RequestHandler()
-# response = request_handler.do_request('https://www.google.com')
-#
 # Author: Daniel Alonso Báscones
 # Date: 2022-12-23
 # Project: TFG OLIVIA
 
+import requests
+import random
+from bs4 import BeautifulSoup
+from typing import Dict
+from modules.Util import *
+
 class RequestHandler(requests.Request):
+    ''' 
+    Class to handle HTTP requests in a more transparent way in scraping and denial of service environments
+    by the servers from which the data is requested
+    Basically, it manages the proxies and user agents so that scraping is not detected
+    '''
 
-    # Class constructor
     def __init__(self, max_request=5, *args, **kwargs):
+        '''
+        Class constructor
 
+        Parameters
+        ----------
+        max_request : int, optional
+            Maximum number of requests to be made with the same proxy, by default 5
+        *args : tuple
+            Arguments to be passed to the parent class
+        **kwargs : dict
+            Keyword arguments to be passed to the parent class
+
+        Returns
+        -------
+            None
+        '''
 
         # Call the constructor of the parent class
         super().__init__(*args, **kwargs)
 
-        # Initialize the proxy list
-        self.proxies = {}
+        self.proxies = {}                   # Initialize the proxy list
+        self.user_agents = []               # Initialize the user agent list
+        self.max_request = max_request      # Maximum number of requests to be made with the same proxy
 
-        # Initialize the user agent list
-        self.user_agents = []
-
-        # Maximum number of requests to be made with the same proxy
-        self.max_request = max_request
-
-    # Get proxies from proxyscrape.com API using the free plan and save them in the proxy list
     def __obtain_proxies(self) -> None:
+        '''
+        Get proxies from proxyscrape.com API using the free plan and save them in the proxy list
+        '''
 
         # get proxy
         proxies = requests.get('https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=10000&country=all&ssl=all&anonymity=all').text
@@ -44,13 +52,15 @@ class RequestHandler(requests.Request):
         # Save (proxy, number_uses) in proxy list
         self.proxies = [(f'http://{proxy}', 0) for proxy in proxies]
 
-    # Get next proxy
     def __get_next_proxy(self) -> Dict[str, str]:
         '''
-        Get a random proxy from the proxy list
-        
-        Returns:
-            dict: Dictionary with the selected proxy
+        Get next proxy from the proxy list
+
+        Returns
+        -------
+        Dict[str, str]
+            Dictionary with the proxy, in the format {'http': 'http://ip:port'} used in the requests library
+
         '''
 
         # If the proxy list is empty, get new proxies
@@ -65,9 +75,7 @@ class RequestHandler(requests.Request):
         if num_usages == self.max_request:
 
             # Remove the tuple proxy from the proxy list
-            self.proxies.remove(
-                (selected_proxy, num_usages-1)
-            )
+            self.proxies.remove((selected_proxy, num_usages-1))
 
         # If not, update the number of proxy uses
         else:
@@ -76,20 +84,16 @@ class RequestHandler(requests.Request):
         # return proxy
         return { 'http': selected_proxy }
 
-    # Get user agents from the useragentstring.com API
     def __obtain_user_agents(self, max_count=30) -> None:
         '''
-        Get user agents from the useragentstring.com API
+        Get user agents from the useragentstring.com API and save them in the user agent list
         '''
-        # Obtener user agents
+        # obtenerUserAgents
         user_agents_request = requests.get('https://www.useragentstring.com/pages/useragentstring.php?name=All').text
         soup = BeautifulSoup(user_agents_request, 'html.parser')
 
-        # Find the div element with id = liste
-        div = soup.find(id="liste")
-
-        # Search for all li elements within the div element
-        lis = div.find_all("li")
+        div = soup.find(id="liste")     # Find the div element with id = liste
+        lis = div.find_all("li")        # Search for all li elements within the div element
 
         # Stores user agents in a list
         count = 0
@@ -98,37 +102,34 @@ class RequestHandler(requests.Request):
             # You only get the first 30 user agents
             if count == max_count:
                 break
-            # Extracts the text of each li element
-            user_agent = li.text
 
-            # Add the user agent to the list
+            # Add the user agent to the list            
+            user_agent = li.text
             self.user_agents.append(user_agent)
             count+=1
 
-    # Get a random user agent from the user agent list
     def __get_random_user_agent(self) -> Dict[str, str]:
         '''
-        Obtener un user agent aleatorio
-        
-        Returns:
-            str: User agent
+        Get a random user agent from the user agent list
+
+        Returns
+        -------
+        Dict[str, str]
+            Dictionary with the user agent, in the format {'User-Agent': 'user agent'} used in the requests library
         '''
-        # Si la lista de user agents está vacía, obtener nuevos user agents
+
+        # If the User Agents list is empty, get new user agents
         if self.user_agents == []:
             self.__obtain_user_agents()
-
-        # Seleccionar un user agent aleatorio
-        selected_user_agent = random.choice(self.user_agents)
-
-        # Eliminar el user agent de la lista
-        self.user_agents.remove(selected_user_agent)
+        
+        selected_user_agent = random.choice(self.user_agents)       # Select a random UserAgent
+        self.user_agents.remove(selected_user_agent)                # Delete the User Agent from the list
 
         return {'User-Agent': selected_user_agent}
 
-    # Make an HTTP request
     def do_request(self, url, retry = False) -> bytes:
         '''
-        Make an HTTP request
+        Make an HTTP request and return the HTML of the response
 
         args:
             url (str): URL of the request
@@ -151,14 +152,13 @@ class RequestHandler(requests.Request):
                 # If the request fails, retry it
                 while response.status_code != 200:
 
-                    # Color red
-                    print(Fore.RED)
-                    print("Request failed:")
-                    print("Proxy: ", proxy)
-                    print("User agent: ", user_agent)
-                    print("URL: ", url)
-                    print("Status code: ", response.status_code)
-                    print("Retrying request. Times: ", retry_count)
+                    print_colored("Request failed as response:", RED)
+                    print_colored("Proxy: " + str(proxy), RED)
+                    print_colored("User agent: " + str(user_agent), RED)
+                    print_colored("URL: " + url, RED)
+                    print_colored("Error: " + str(response.status_code), RED)
+                    print_colored("Retrying request", RED)
+                    
                     response = requests.get(url, proxies=proxy, headers=user_agent, timeout=10)
 
                     # If the request fails 5 times in a row, change the proxy and user agent
@@ -166,39 +166,26 @@ class RequestHandler(requests.Request):
                         print("Request failed 5 times. Changing proxy and user agent")
                         proxy = self.__get_next_proxy()
                         user_agent = self.__get_random_user_agent()
-
-                    # Color reset
-                    print(Style.RESET_ALL)
-                    
+             
                     # Increment retry count
                     retry_count+=1
 
         except Exception as e:
+
             # Color red
-            print(Fore.RED)
-            print("Request failed:")
-            print("Proxy: ", proxy)
-            print("User agent: ", user_agent)
-            print("URL: ", url)
-            print("Exception: ", e)
-            print("Retrying request")
-            print(Style.RESET_ALL)
+            print_colored("Request failed as Exception:", RED)
+            print_colored("Proxy: " + str(proxy), RED)
+            print_colored("User agent: " + str(user_agent), RED)
+            print_colored("URL: " + url, RED)
+            print_colored("Error: " + str(e), RED)
+            print_colored("Retrying request", RED)
 
-
-            # Get proxy and user agent
+            # Renew proxy and user agent
             proxy = self.__get_next_proxy()
             user_agent = self.__get_random_user_agent()
 
             # If the request fails, retry it
             response = self.do_request(url, retry=True)
+
         # return HTML
         return response
-    
-
-# Example
-if __name__ == "__main__":
-    request_handler = RequestHandler()
-    response = request_handler.do_request('https://www.google.com')
-    print(response.text)
-
-    
