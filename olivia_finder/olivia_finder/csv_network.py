7cc6f1c11dac699a2a04deebbb6f6a6471e07b8d
Daniel Alonso <dab0012@alu.ubu.es>
2023-03-10 20:25:09 +0100

    Updated sonnarclud properties file #8

--- olivia_finder/olivia_finder/csv_network.py
+++ olivia_finder/olivia_finder/csv_network.py
@@ -18,52 +18,45 @@ class CSVNetwork(DataSource):
 
     # Methods
     # ---------------------
-    def __init__(self, name = None, description = None):
-        """
-        Constructor
+    def __init__(self, name:str = None, description:str = None):
+        """Constructor method.
 
-        ---
-        Parameters
-        -   name: str -> Name of the data source.
-        -   description: str -> Description of the data source.
+        :param name: name of the data source, defaults to None
+        :type name: str, optional
+        :param description: _description_, defaults to None
+        :type description: str, optional
         """
         super.__init__(name, description)
 
-    def load_data(self, file_path = None) -> None:
-        """
-        Loads the data from csv file.
+    def load_data(self, file_path:str) -> None:
+        """Loads the data from a CSV file.
 
-        ---
-        Parameters
-        -  file_path : str (Path to the file to load)
-
-        Returns
-        -   None
+        :param file_path: the path of the file to load, defaults to None
+        :type file_path: str, optional
+        :raises ValueError: when file path is None
+        :raises FileNotFoundError: when file is not found
+        :raises ValueError: when file is not a CSV file
         """
 
-        # If file_path is not defined, use the default one
         if file_path is None:
-            raise ValueError("persistence_path is not defined.")
+            raise ValueError("File path cannot be None.")
         
-        # Read file as a pandas dataframe if it exists
-        if os.path.exists(file_path):
-            self.data = pd.read_csv(file_path)
-        else:
+        if not os.path.exists(file_path):
             raise FileNotFoundError(f"File {file_path} not found.")
-
-        # If persistence_path is still not defined, raise an error
-        if file_path is None:
-            raise ValueError("persistence_path is not defined.")
+        
+        if not file_path.endswith(".csv"):
+            raise ValueError(f"File {file_path} is not a CSV file.")
+        
+        self.data = pd.read_csv(file_path)
 
     @override
     def obtain_package_names(self) -> List[str]:
         """
         Obtains the list of packages from the data source.
 
-        ---
-        Returns
-        -   List[str] -> List of package names.
+        :return: list of package names
         """
+        
         pass
 
     def obtain_package_data(self, package_name: str) -> Package:
@@ -86,11 +79,15 @@ class CSVNetwork(DataSource):
             raise ValueError(f"Package {package_name} not found.")
 
         # Create the package object
-        package = Package(package_name, row['version'].values[0], row['description'].values[0], row['url'].values[0], row['license'].values[0], row['dependencies'].values[0])
-
-        return package
-
-    
+        return Package(
+            package_name, 
+            row['version'].values[0], 
+            row['description'].values[0], 
+            row['url'].values[0], 
+            row['license'].values[0], 
+            row['dependencies'].values[0]
+        )
+   
     @abstractmethod
     def obtain_packages_data(self, pckg_names: List[str] = None, progress = None) -> List[Package]:
         '''
