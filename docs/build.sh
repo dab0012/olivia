893485df75df047fa9aa8fa89395655690709e45
Daniel Alonso <dab0012@alu.ubu.es>
2023-05-14 21:56:06 +0200

    Adedd logo to doc #19

--- docs/build.sh
+++ docs/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pdoc -o docs olivia_finder/olivia_finder --footer-text "Olivia Finder Doc, by Daniel Alonso Báscones" 
+pdoc -o docs olivia_finder/olivia_finder --footer-text "Olivia Finder Doc, by Daniel Alonso Báscones"  --logo https://raw.githubusercontent.com/dab0012/olivia-finder/master/docs/img/logo.png --favicon https://raw.githubusercontent.com/dab0012/olivia-finder/master/docs/img/favicon.ico
